8b54e6e0d474d236818233f188978446d7b44373
phi <philipwaritschlager@web.de>
2018-01-19 17:45:21 +0100

    synchronous downloading print progress bugfix attachments

--- crawler.js
+++ crawler.js
@@ -80,11 +80,11 @@ function parseMessage(messageHtml) {
                 const n = fn(elem.children, options);
                 return `<a href="${elem.attribs.href}" alt="${elem.attribs.alt}">${n}</a>`; // preserve links as html
             },
-            image: (elem, fn, options) => {
+            image: (elem) => {
                 if (elem.attribs.src.match(/\/images\/smil(ie|ey)s\//)) {
-                    return elem.attribs.alt;
+                    return elem.attribs.alt; // woltlab wcf smileys: pure alt
                 }
-                // download img todo
+                // download this external img maybe todo
                 return `<img src="${elem.attribs.src}" alt="${elem.attribs.alt}" />`; // preserve
             },
             heading: (elem, fn, options) => {
@@ -109,10 +109,10 @@ function parseTime(timestring) {
         [, month, day, year, hour, min] = match;
     } else {
         match = /^([A-z]+), ([0-9]+):([0-9]+)[A-z]+$/.exec(timestring);
-        [, day, hour, min] = match;
         if (match === null) {
             error(`cannot parse date ${timestring}`);
         } else {
+            [, day, hour, min] = match;
             const date = new Date();
             if (day === 'Yesterday') {
                 date.setDate(date.getDate() - 1);
@@ -135,7 +135,7 @@ function parseTime(timestring) {
 
 const visitedThreads = new Set();
 
-async function downloadThread(baseUrl, path, pageMax) {
+async function downloadThread(baseUrl, path, pageMax, progressString) {
     if (visitedThreads.has(baseUrl)) {
         error(`[thread] skipping: ${path}`);
         return;
@@ -152,16 +152,17 @@ async function downloadThread(baseUrl, path, pageMax) {
         pageUrls.push(`${baseUrl}index${i}.html`);
     }
 
-    // visit pages & save
-    await pageUrls.forEach(async (pageUrl) => { // todo to await or not to async
+    // visit pages iteratively & save
+    for (const [pageIndex, pageUrl] of pageUrls.entries()) {
+        log(`${progressString}, page ${pageIndex + 1}/${pageUrls.length}`);
         const jPageHtml = $(await getHtml(pageUrl));
+        // save all messages to db
         jPageHtml.find('#main > .message > .messageInner')
             .each((_, divMessage) => {
                 const jDivMessage = $(divMessage);
                 const timestring = jDivMessage.find('> div.messageContent > div.messageContentInner > div.messageHeader > div.containerContent > p.smallFont.light').text();
-                // const timestamp = parseTime(timestring) / 1000; // FIXME
-                const timestamp = 1000000000;
-                // const postNumber = jDivMessage.find('> div.messageContent > div.messageContentInner > div.messageHeader > p.messageCount > a').text();
+                const timestamp = parseTime(timestring) / 1000;
+                // const timestamp = 1000000000; // FIXME
                 const username = jDivMessage.find('> div.messageSidebar > div.messageAuthor > p.userName > a > span').text();
                 const messageHtml = jDivMessage.find('> div.messageContent > div.messageContentInner > div.messageBody > div').html();
                 let message = parseMessage(messageHtml);
@@ -171,24 +172,22 @@ async function downloadThread(baseUrl, path, pageMax) {
                     .each((__, aAttachment) => {
                         const attachmentHref = aAttachment.href;
                         const attachmentId = attachmentHref.replace(/^.+&attachmentID=([0-9]+)([^0-9].*$|$)/, '$1');
-                        saveUrl(`attachments/${attachmentHref}`, attachmentId);
-                        message += `\nAttachment:\n<img src="attachments/${attachmentHref}`;
+                        saveUrl(`attachments/${attachmentId}`, attachmentHref); // async in background
+                        message += `\nAttachment:\n<img src="attachments/${attachmentId}`;
                     });
 
-                log(pageUrl, username);
                 db.run('INSERT INTO posts (thread, timestamp, username, message) VALUES (?,?,?,?)', dbThreadId, timestamp, username, message, (errormessage) => {
                     if (errormessage !== null) {
                         error(errormessage);
                     }
                 });
-                return false;
             });
-    });
+    }
 }
 
 const visitedBoards = new Set();
 
-async function iterateBoards(url, path) {
+async function iterateBoards(url, path, currentProgressString) {
     if (visitedBoards.has(url)) {
         error(`[board] skipping: ${path}`);
         return;
@@ -197,14 +196,17 @@ async function iterateBoards(url, path) {
 
     // find sub-boards
     const jHtml = $(await getHtml(url));
-    jHtml
+    const aBoards = jHtml
         .find(`#boardlist > li.border > ul > li > div.boardlistInner > div.boardlistTitle > div.containerContent > .boardTitle > a,
-               #boardlist > li.border > div.boardlistInner:not(.containerHead) > div.boardlistTitle > div.containerContent > .boardTitle > a`)
-        .each((boardIndex, aBoard) => {
-            const board = `${zeropad(boardIndex)} ${aBoard.text}`;
-            const subpath = `${path}/${board}`;
-            iterateBoards(absoluteUrl(aBoard.href), subpath); // recursive
-        });
+               #boardlist > li.border > div.boardlistInner:not(.containerHead) > div.boardlistTitle > div.containerContent > .boardTitle > a`);
+    // iterative for await
+    for (let boardIndex = 0; boardIndex < aBoards.length; boardIndex++) {
+        const jABoard = $(aBoards[boardIndex]);
+        const board = `${zeropad(boardIndex)} ${jABoard.text()}`;
+        const subpath = `${path}/${board}`;
+        const nextProgressString = `${currentProgressString}, board ${boardIndex + 1}/${aBoards.length}`;
+        await iterateBoards(absoluteUrl(jABoard.prop('href')), subpath, nextProgressString); // "synchronous" recursive
+    }
 
     // this board: get page urls
     const pageMax = jHtml.find('#main > div.contentHeader > div.pageNavigation > ul > li:nth-last-child(2) > a').text();
@@ -213,21 +215,22 @@ async function iterateBoards(url, path) {
         pageUrls.push(`${url}index${i}.html`);
     }
     // visit all pages of board
-    pageUrls.forEach(async (pageUrl) => {
+    for (const [pageIndex, pageUrl] of pageUrls.entries()) {
         const jPageHtml = $(await getHtml(pageUrl));
-        jPageHtml.find('div:not(#topThreadsStatus):not(.tabMenuContent) > table.tableList > tbody > tr > td.columnTopic')
-            .each((topicIndex, tdTopic) => {
-                const jTdTopic = $(tdTopic);
+        const tdTopics = jPageHtml.find('div:not(#topThreadsStatus):not(.tabMenuContent) > table.tableList > tbody > tr > td.columnTopic');
+        for (let topicIndex = 0; topicIndex < tdTopics.length; topicIndex++) {
+            const jTdTopic = $(tdTopics[topicIndex]);
 
-                const topicPageMax = jTdTopic.find('> div.statusDisplay > div.pageNavigation > ul > li:last-child() > a').text(); // empty or 2+
+            const topicPageMax = jTdTopic.find('> div.statusDisplay > div.pageNavigation > ul > li:last-child() > a').text(); // empty or 2+
 
-                const aTopic = jTdTopic.find('> div.topic > p > a')[0];
-                const topic = `${zeropad(topicIndex)} ${aTopic.text}`;
-                const subpath = `${path}/${topic}`;
+            const aTopic = jTdTopic.find('> div.topic > p > a')[0];
+            const topic = `${zeropad(topicIndex)} ${aTopic.text}`;
+            const subpath = `${path}/${topic}`;
 
-                downloadThread(absoluteUrl(aTopic.href), subpath, topicPageMax);
-            });
-    });
+            const progressString = `${currentProgressString}, page ${pageIndex + 1}/${pageUrls.length}, thread ${topicIndex + 1}/${tdTopics.length}`;
+            await downloadThread(absoluteUrl(aTopic.href), subpath, topicPageMax, progressString);
+        }
+    }
 }
 
 process.on('exit', () => {
@@ -236,5 +239,5 @@ process.on('exit', () => {
 });
 (async function main() {
     log('starting');
-    await iterateBoards(rootUrl, '.');
+    await iterateBoards(rootUrl, '.', 'root');
 }());
